b0c369833a7d08ef749720e6f445b4768d09fb6b
DGCK81LNN <54282183+DGCK81LNN@users.noreply.github.com>
2024-02-06 19:30:16 +0800

    chore: update dict to ver 240206

--- site/app.vue
+++ site/app.vue
@@ -29,6 +29,12 @@
     <aside>
       <h2 class="h4">更新记录</h2>
       <dl class="changelog mb-0">
+        <dt>2024-02-06</dt>
+        <dd>
+          <ul>
+            <li>字表更新至 240206 版。</li>
+          </ul>
+        </dd>
         <dt>2024-02-05</dt>
         <dd>
           <ul>
@@ -110,7 +116,7 @@
     <hr class="d-md-none" />
     <footer class="text-md-end">
       <p>DGCK81LNN 制作</p>
-      <p>〇改希顶语字表更新至 240205 版</p>
+      <p>〇改希顶语字表更新至 240206 版</p>
       <p>
         <span id="busuanzi_container_page_pv" style="display: none">
           本页面已被访问 <span id="busuanzi_value_page_pv"></span> 次
